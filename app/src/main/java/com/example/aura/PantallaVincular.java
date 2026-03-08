d5e21011b615f3b2a3ca6e62e992628ab55d1252
Ariel-Munoz-ISC6 <arielalejandromunoz1@gmail.com>
2026-03-07 23:38:59 -0600

    Add PantallaJuegos, PantallaVincular and PantallaCompartirCodigo

---
+++ app/src/main/java/com/example/aura/PantallaVincular.java
@@ -0,0 +1,12 @@
+package com.example.aura;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PantallaVincular extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pantalla_vincular);
+    }
+}
